87646b024cc45ae582de8ec137c0a81f5c0b6c00
kenie <18376792744@163.com>
2022-10-25 16:04:10 +0800

    first commit

---
+++ common/config.js
@@ -0,0 +1,11 @@
+export default {
+	// api请求前缀
+	// webUrl:'https://ceshi2.dishait.cn/api/v1',
+	// webUrl:'http://www.shejiao.com/api/v1',
+	webUrl:'http://chat.cloud-cc.top/api/v1',
+	host: 'http://chat.cloud-cc.top/',
+	// websocket地址
+	// websocketUrl:"wss://ceshi2.dishait.cn/wss",
+	// websocketUrl:"ws://www.shejiao.com/ws",
+	websocketUrl:"ws://chat.cloud-cc.top/wss",
+}
